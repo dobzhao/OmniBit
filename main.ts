6c0eccd5f53f265e514ef0a6da47e1298f046c70
dob <dob@pocuta.com>
2020-08-27 20:38:08 +0800

    小车上增加超声传感器，从鳄鱼板api中拷贝相关函数

--- main.ts
+++ main.ts
@@ -350,6 +350,26 @@ namespace OmniBit {
         }
     }
 
+    //% blockId=CrocoKit_Sensor_Ultrasonic block="Ultrasonic|Trig %Trig|Echo %Echo"
+    //% color="#228B22"
+    //% weight=97
+    //% blockGap=20
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=5
+    export function Ultrasonic(Trig: DigitalPin, Echo: DigitalPin): number {
+        //send pulse
+        pins.setPull(Trig, PinPullMode.PullNone);
+        pins.digitalWritePin(Trig, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(Trig, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(Trig, 0);
+
+        //read pulse, maximum distance=500cm
+        const d = pins.pulseIn(Echo, PulseValue.High, 500 * 58);   
+
+        return Math.idiv(d, 58);
+    }
+
     //% blockId=OmniBit_Polygon block="Polygon|%polygon|speed %speed"
     //% weight=101
     //% blockGap=10
